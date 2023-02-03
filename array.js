d2af8df9455acbd7bc9c1dadd03e8adad1690ba3
Sanzanaimaam <sanzanaimaam@gmail.com>
2023-02-03 20:08:23 +0600

    array

--- array.js
+++ array.js
@@ -1,4 +1,12 @@
-function go(){
-
-}
-go()
+const country='Bangladesh';
+const age=78;
+const student={id:20, name:'sanzana' ,class:11};
+const friends=[2,33,55,66,88]
+const newFriends=['a', 'v','k', 't']
+console.log(typeof country)
+console.log(typeof age)
+console.log(typeof student)
+console.log(Array.isArray(friends))
+const friendsList=newFriends.concat(friends);
+console.log(friendsList);
+console.log(friends.includes(90))
